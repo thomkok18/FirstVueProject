ff79aacff9f5c992e9f674856584a2b5b9b06c62
thomkok20 <tkok@roc-dev.com>
2019-01-22 23:54:45 +0100

    first commit

---
+++ src/App.vue
@@ -0,0 +1,46 @@
+<template>
+    <div id="app">
+        <nav>
+            <router-link to="/">Home</router-link>
+            <router-link to="/about">About</router-link>
+        </nav>
+
+        <router-view />
+    </div>
+</template>
+
+<script>
+    export default {
+        name: 'app'
+    }
+</script>
+
+<style>
+    @import url('https://fonts.googleapis.com/css?family=Montserrat:400,700');
+
+    body {
+        background-color: #EEEEEE;
+        font-family: 'Montserrat', sans-serif;
+        display: grid;
+        grid-template-rows: auto;
+        justify-items: center;
+        padding-top: 50px;
+    }
+    body, html {
+        margin: 0;
+        height: 100%;
+    }
+    #app {
+        width: 50%;
+    }
+
+    nav a {
+        padding: 10px;
+        text-decoration: none;
+        background: #fff;
+        border-radius: 3px;
+        color: rgb(0, 110, 255);
+        font-weight: bold;
+        margin-right: 15px;
+    }
+</style>
---
+++ src/components/About.vue
@@ -0,0 +1,17 @@
+<template>
+    <div class="about">
+        <h1>Hello {{their_name}}, This is an about page</h1>
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+    </div>
+</template>
+
+<script>
+    export default {
+
+        data() {
+            return {
+                their_name: this.$route.params.name,
+            }
+        }
+    }
+</script>
